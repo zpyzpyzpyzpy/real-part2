d522cb134b5e1bbfc16113f128ac86c363af0a18
zpyzpyzpyzpy <79196475+zpyzpyzpyzpy@users.noreply.github.com>
2022-11-29 11:22:40 +0000

    Update part2_test.c

--- part2_test.c
+++ part2_test.c
@@ -25,135 +25,391 @@
  * 1 tab == 4 spaces!
  */
 
-/******************************************************************************
- * This project provides two demo applications.  A simple blinky style project,
- * and a more comprehensive test and demo application.  The
- * configCREATE_SIMPLE_TICKLESS_DEMO setting (defined in FreeRTOSConfig.h) is
- * used to select between the two.  The simply blinky demo is implemented and
- * described in main_blinky.c.  The more comprehensive test and demo application
- * is implemented and described in main_full.c.
- *
- * The comprehensive demo uses FreeRTOS+CLI to create a simple command line
- * interface through a UART.
- *
- * The blinky demo uses FreeRTOS's tickless idle mode to reduce power
- * consumption.  See the notes on the web page below regarding the difference
- * in power saving that can be achieved between using the generic tickless
- * implementation (as used by the blinky demo) and a tickless implementation
- * that is tailored specifically to the MSP432.
- *
- * This file implements the code that is not demo specific.
- *
- * See http://www.FreeRTOS.org/TI_MSP432_Free_RTOS_Demo.html for instructions.
- *
- */
+// Author:      Mohd A. Zainol
+// Date:        1 Oct 2018
+// Chip:        MSP432P401R LaunchPad Development Kit (MSP-EXP432P401R) for TI-RSLK
+// File:        main_program.c
+// Function:    The main function of our code in FreeRTOS
 
 /* Standard includes. */
 #include <stdio.h>
 
 /* Kernel includes. */
+#include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
-/* Song file */
+/* TI includes. */
+#include "gpio.h"
+
+/* ARM Cortex */
 #include <stdint.h>
 #include "msp.h"
-#include "../inc/CortexM.h"
-#include "../inc/SysTick.h"
-#include "../inc/songFile.h"
+#include "SysTick.h"
+#include "inc/CortexM.h"
 
-/*-----------------------------------------------------------*/
+// TODO: include the songFile header file
+#include "./inc/songFile.h"
+// TODO: include the dcMotor header file
+#include "./inc/dcMotor.h"
+// TODO: include the bumpSwitch header file
+#include "./inc/bumpSwitch.h"
+// TODO: include the outputLED header file
+#include "./inc/outputLED.h"
+// TODO: include the SysTick header file
+#include "./inc/SysTick.h"
+// TODO: bit-banded addresses positive logic of input switch S1
+#define SW1IN ((*((volatile uint8_t *)(0x42098004)))^1)
+#define SW2IN ((*((volatile uint8_t *)(0x42098010)))^1)
+#define REDLED (*((volatile uint8_t *)(0x42098040)))    // output: red LED
+// TODO: bit-banded addresses positive logic of input switch S2
+
+// TODO: declare a global variable to read bump switches value,
+//       name this as bumpSwitch_status and use uint8_t
+uint8_t bumpSwitch_status;
+
+// static void Switch_Init
+static void Switch_Init(void);
+
+// a static void function for a task called "taskMasterThread"
+static void taskMasterThread( void *pvParameters );
+// TODO: declare a static void function for a task called "taskBumpSwitch"
+static void taskBumpSwitch(void *pvParameters);
+// TODO: declare a static void function for a task called "taskPlaySong"
+static void taskPlaySong(void *pvParameters);
+// TODO: declare a static void function for a task called "taskdcMotor"
+static void taskdcMotor(void *pvParameters);
+// TODO: declare a static void function for a task called "taskReadInputSwitch"
+static void taskReadInputSwitch(void *pvParameters);
+// TODO: declare a static void function for a task called "taskdcMotor"
+static void taskDisplayOutputLED(void *pvParameters);
+/*
+ * Called by main() to create the main program application
+ */
+void main_program( void );
 
 /*
- * Set up the hardware ready to run this demo.
+ * The configuration of clocks for frequency.
  */
-static void prvSetupHardware( void );
+static void prvConfigureClocks( void );
 
-extern void main_program( void );
-
-/*-----------------------------------------------------------*/
-
-int main( void )
+// declare an identifier of task handler called "taskHandle_BlinkRedLED"
+xTaskHandle taskHandle_BlinkRedLED;
+// TODO: declare an identifier of task handler called "taskHandle_BumpSwitch"
+xTaskHandle taskHandle_BumpSwitch;
+// TODO: declare an identifier of task handler called "taskHandle_PlaySong"
+xTaskHandle taskHandle_PlaySong;
+// TODO: declare an identifier of task handler called "taskHandle_dcMotor"
+xTaskHandle taskHandle_dcMotor;
+// TODO: declare an identifier of task handler called "taskHandle_InputSwitch"
+xTaskHandle taskHandle_InputSwitch;
+// TODO: declare an identifier of task handler called "taskHandle_OutputLED"
+xTaskHandle taskHandle_OutputLED;
+void main_program( void )
 {
-    /* See http://www.FreeRTOS.org/TI_MSP432_Free_RTOS_Demo.html for instructions. */
+    // initialise the clock configuration
+    prvConfigureClocks();
+    // TODO: initialise the switch
+    Switch_Init();
+    // TODO: initialise systick timer
+    SysTick_Init();
+    //////////////////////////////////////////////////////
+    // TIP: to create a task, use xTaskCreate in FreeRTOS
+    // URL : https://www.freertos.org/a00125.html
+    //////////////////////////////////////////////////////
 
-    /* Prepare the hardware to run this demo. */
-    prvSetupHardware();
+        // TODO: Create a task that has these parameters=
+        //       pvTaskCode: taskMasterThread
+        //       pcName: taskT
+        //       usStackDepth: 128
+        //       pvParameters: NULL
+        //       uxPriority: 2
+        //       pxCreatedTask: taskHandle_BlinkRedLED
+        xTaskCreate(
+                taskMasterThread,
+                "taskT",
+                128,
+                NULL,
+                2,
+                &taskHandle_BlinkRedLED);
 
-    main_program();
+        // TODO: Create a task that has these parameters=
+        //       pvTaskCode: taskBumpSwitch
+        //       pcName: taskB
+        //       usStackDepth: 128
+        //       pvParameters: NULL
+        //       uxPriority: 1
+        //       pxCreatedTask: taskHandle_BumpSwitch
+        xTaskCreate(
+                taskBumpSwitch,
+                "taskB",
+                128,
+                NULL,
+                1,
+                &taskHandle_BumpSwitch);
 
-    return 0;
-}
-/*-----------------------------------------------------------*/
 
-static void prvSetupHardware( void )
-{
-extern void FPU_enableModule( void );
+        // TODO: Create a task that has these parameters=
+        //       pvTaskCode: taskPlaySong
+        //       pcName: taskS
+        //       usStackDepth: 128
+        //       pvParameters: NULL
+        //       uxPriority: 1
+        //       pxCreatedTask: taskHandle_PlaySong
+        xTaskCreate(
+                taskPlaySong,
+                "taskS",
+                128,
+                NULL,
+                1,
+                &taskHandle_PlaySong);
 
-    /* The clocks are not configured here, but inside main_full() and
-    main_blinky() as the full demo uses a fast clock and the blinky demo uses
-    a slow clock. */
+        // TODO: Create a task that has these parameters=
+        //       pvTaskCode: taskdcMotor
+        //       pcName: taskM
+        //       usStackDepth: 128
+        //       pvParameters: NULL
+        //       uxPriority: 1
+        //       pxCreatedTask: taskHandle_dcMotor
+        xTaskCreate(
+                taskdcMotor,
+                "taskM",
+                128,
+                NULL,
+                1,
+                &taskHandle_dcMotor);
+        // TODO: Create a task that has these parameters=
+        //       pvTaskCode: taskReadInputSwitch
+        //       pcName: taskR
+        //       usStackDepth: 128
+        //       pvParameters: NULL
+        //       uxPriority: 1
+        //       pxCreatedTask: taskHandle_InputSwitch
+        xTaskCreate(
+                taskReadInputSwitch,
+                "taskR",
+                128,
+                NULL,
+                1,
+                &taskHandle_InputSwitch);
+        // TODO: Create a task that has these parameters=
+        //       pvTaskCode: taskDisplayOutputLED
+        //       pcName: taskD
+        //       usStackDepth: 128
+        //       pvParameters: NULL
+        //       uxPriority: 1
+        //       pxCreatedTask: taskHandle_OutputLED
+        xTaskCreate(
+                taskDisplayOutputLED,
+                "taskD",
+                128,
+                NULL,
+                1,
+                &taskHandle_OutputLED);
+        //////////////////////////////////////////////////////////////////
+        // TIP: to start a scheduler, use vTaskStartScheduler in FreeRTOS
+        // URL : https://www.freertos.org/a00132.html
+        //////////////////////////////////////////////////////////////////
 
-    /* Stop the watchdog timer. */
-    MAP_WDT_A_holdTimer();
+        // TODO: start the scheduler
+        vTaskStartScheduler();
 
-    /* Ensure the FPU is enabled. */
-    FPU_enableModule();
-
-}
-/*-----------------------------------------------------------*/
-
-void vApplicationMallocFailedHook( void )
-{
-    /* vApplicationMallocFailedHook() will only be called if
-    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-    function that will get called if a call to pvPortMalloc() fails.
-    pvPortMalloc() is called internally by the kernel whenever a task, queue,
-    timer or semaphore is created.  It is also called by various parts of the
-    demo application.  If heap_1.c or heap_2.c are used, then the size of the
-    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-    to query the size of free heap space that remains (although it does not
-    provide information on how the remaining heap might be fragmented). */
-    taskDISABLE_INTERRUPTS();
+    /* INFO: If everything is fine, the scheduler will now be running,
+    and the following line will never be reached.  If the following line
+    does execute, then there was insufficient FreeRTOS heap memory
+    available for the idle and/or timer tasks to be created. See the
+    memory management section on the FreeRTOS web site for more details. */
     for( ;; );
 }
-/*-----------------------------------------------------------*/
-
-void vApplicationIdleHook( void )
+/*-----------------------------------------------------------------*/
+/*------------------- FreeRTOS configuration ----------------------*/
+/*-------------   DO NOT MODIFY ANYTHING BELOW HERE   -------------*/
+/*-----------------------------------------------------------------*/
+// The configuration clock to be used for the board
+static void prvConfigureClocks( void )
 {
-    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-    task.  It is essential that code added to this hook function never attempts
-    to block in any way (for example, call xQueueReceive() with a block time
-    specified, or call vTaskDelay()).  If the application makes use of the
-    vTaskDelete() API function (as this demo application does) then it is also
-    important that vApplicationIdleHook() is permitted to return to its calling
-    function, because it is the responsibility of the idle task to clean up
-    memory allocated by the kernel to any task that has since been deleted. */
-}
-/*-----------------------------------------------------------*/
+    // Set Flash wait state for high clock frequency
+    FlashCtl_setWaitState( FLASH_BANK0, 2 );
+    FlashCtl_setWaitState( FLASH_BANK1, 2 );
 
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+    // This clock configuration uses maximum frequency.
+    // Maximum frequency also needs more voltage.
+
+    // From the datasheet: For AM_LDO_VCORE1 and AM_DCDC_VCORE1 modes,
+    // the maximum CPU operating frequency is 48 MHz
+    // and maximum input clock frequency for peripherals is 24 MHz.
+    PCM_setCoreVoltageLevel( PCM_VCORE1 );
+    CS_setDCOCenteredFrequency( CS_DCO_FREQUENCY_48 );
+    CS_initClockSignal( CS_HSMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+    CS_initClockSignal( CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+    CS_initClockSignal( CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+    CS_initClockSignal( CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+}
+
+// The sleep processing for MSP432 board
+void vPreSleepProcessing( uint32_t ulExpectedIdleTime ){}
+
+#if( configCREATE_SIMPLE_TICKLESS_DEMO == 1 )
+    void vApplicationTickHook( void )
+    {
+        /* This function will be called by each tick interrupt if
+        configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+        added here, but the tick hook is called from an interrupt context, so
+        code must not attempt to block, and only the interrupt safe FreeRTOS API
+        functions can be used (those that end in FromISR()). */
+        /* Only the full demo uses the tick hook so there is no code is
+        executed here. */
+    }
+#endif
+/*-----------------------------------------------------------------*/
+/*-------------   DO NOT MODIFY ANYTHING ABOVE HERE   -------------*/
+/*--------------------------- END ---------------------------------*/
+/*-----------------------------------------------------------------*/
+
+static void Switch_Init(void){
+    // negative logic built-in Button 1 connected to P1.1
+    // negative logic built-in Button 2 connected to P1.4
+    P1->SEL0 &= ~0x12;
+    P1->SEL1 &= ~0x12;      // configure P1.4 and P1.1 as GPIO
+    P1->DIR &= ~0x12;       // make P1.4 and P1.1 in
+    P1->REN |= 0x12;        // enable pull resistors on P1.4 and P1.1
+    P1->OUT |= 0x12;        // P1.4 and P1.1 are pull-up
+}
+// a static void function for taskReadInputSwitch
+static void taskReadInputSwitch( void *pvParameters ){
+
+    char i_SW1=0;
+    int i;
+
+    for( ;; )
+    {
+        if (SW1IN == 1) {
+            i_SW1 ^= 1;                 // toggle the variable i_SW1
+            for (i=0; i<1000000; i++);  // this waiting loop is used
+                                        // to prevent the switch bounce.
+        }
+
+        ///////////////////////////////////////////////////////////
+        // TIP: to suspend a task, use vTaskSuspend in FreeRTOS
+        // URL: https://www.freertos.org/a00130.html
+        ///////////////////////////////////////////////////////////
+
+        ///////////////////////////////////////////////////////////
+        // TIP: to resume a task, use vTaskResume in FreeRTOS
+        // URL: https://www.freertos.org/a00131.html
+        ///////////////////////////////////////////////////////////
+
+        if (i_SW1 == 1) {
+            REDLED = 1;     // turn on the red LED
+            // TODO: suspend the task taskHandle_PlaySong
+            vTaskSuspend(taskHandle_PlaySong);
+
+        }
+        else {
+            REDLED = 0;     // turn off the red LED
+            // TODO: resume the task taskHandle_PlaySong
+            vTaskResume(taskHandle_PlaySong);
+
+        }
+
+    }
+}
+
+// TODO: create a static void function for taskPlaySong
+
+    // TODO: initialise the song
+
+    // TODO: play the song's function and run forever
+
+static void taskPlaySong(void *pvParameters)
 {
-    ( void ) pcTaskName;
-    ( void ) pxTask;
-
-    /* Run time stack overflow checking is performed if
-    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-    function is called if a stack overflow is detected. */
-    taskDISABLE_INTERRUPTS();
-    for( ;; );
+    init_song_pwm();
+        while(1)
+        {
+            play_song();
+        }
 }
-/*-----------------------------------------------------------*/
+// TODO: create a static void function for taskBumpSwitch
+    // TODO: initialise bump switches
 
-void *malloc( size_t xSize )
+    // TODO: Read the input of bump switches forever:
+    //       Continuously read the 6 bump switches in a loop,
+    //       and return it to the "bumpSwitch_status" variable.
+    //       Note that the bumpSwitch_status is a global variable,
+    //       so do not declare it again here locally.
+
+static void taskBumpSwitch(void *pvParameters)
 {
-    /* There should not be a heap defined, so trap any attempts to call
-    malloc. */
-    Interrupt_disableMaster();
-    for( ;; );
+      BumpSwitch_Init();
+      for( ;; ) // uncomment this
+      { // uncomment this
+          bumpSwitch_status=Bump_Read_Input() ;
+        // TODO: use bumpSwitch_status as the variable and
+        //       use Bump_Read_Input to read the input
+
+      } // uncomment this
 }
-/*-----------------------------------------------------------*/
+// TODO: create a static void function for taskDisplayOutputLED
+static void taskDisplayOutputLED(void *pvParameters)
+{
+    for( ;; ) // uncomment this
+    { // uncomment this
+        outputLED_response(bumpSwitch_status);
+        // TODO: use outputLED_response as the function and
+        //       use bumpSwitch_status as the parameter
+
+    } // uncomment this
+}
+// a static void function for taskMasterThread
+static void taskMasterThread( void *pvParameters )
+{
+    int i;
+
+    // TODO: initialise the color LED
+    ColorLED_Init();
+    // initialise the red LED
+    RedLED_Init();
+
+    while(!SW2IN){                  // Wait for SW2 switch
+        for (i=0; i<1000000; i++);  // Wait here waiting for command
+        REDLED = !REDLED;           // The red LED is blinking
+    }
+
+    // TODO: Turn off the RED LED, we no longer need that.
+        REDLED=0;
+    //////////////////////////////////////////////////////////////////
+    // TIP: to suspend a task, use vTaskSuspend in FreeRTOS
+    // URL: https://www.freertos.org/a00130.html
+    //////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////
+    // TIP: to delete a task, use vTaskDelete in FreeRTOS
+    // URL: https://www.freertos.org/a00126.html
+    //////////////////////////////////////////////////////////////////
+        vTaskSuspend(taskHandle_BlinkRedLED);
+}
+    // TODO: This function (taskMasterThread)is no longer needed.
+    //       Please suspend this task itself, or maybe just delete it.
+    //       Question: what are the difference between 'suspend' the task,
+    //                 or 'delete' the task?
+
+
+
+// TODO: create a static void function for taskdcMotor
+
+    // TODO: initialise the DC Motor
+
+    // TODO: use a polling that continuously read from the bumpSwitch_status,
+    //       and run this forever in a while loop.
+    //       use dcMotor_response and bumpSwitch_status for the parameter
+static void taskdcMotor( void *pvParameters )
+{
+    dcMotor_Init();
+            while(1)
+            {
+                dcMotor_response(bumpSwitch_status);
+            }
+}
 
 
